375eecf6ddbb36c460ffb53dfd7d12ef88db466f
B Tasker <github@bentasker.co.uk>
2021-01-03 10:19:15 +0000

    Add initial implementation.
    This gives us a basic tarpit which will write out 10 ascii chars, sleep for 2s and then repeat
    
    It's enough to keep the OpenSSH client tied up, but is fairly formulaic, so I'd like to switch it up a bit in subsequent commits

---
+++ ssh_tarpit.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "net"
+    "strings"
+    "time"
+)
+
+const (
+    LISTEN_PORT = "2222"
+)
+
+func main() {
+    // Start listening on the specified port
+    listener, err := net.Listen("tcp", "0.0.0.0:" + LISTEN_PORT)
+    if err != nil {
+	panic(err)
+    }
+    
+    // Close the listener when the app closes
+    defer listener.Close()
+    fmt.Println("Listening on " + LISTEN_PORT)
+    
+    // Start accepting connections
+    for {
+        conn, err := listener.Accept()
+        if err != nil {
+            // trap error and move onto a new connection
+            continue
+	}        
+        
+        // Handle the connection in a new goroutine
+        go handleConnection(conn)
+    }
+    
+}
+
+
+func handleConnection(conn net.Conn) {
+    // Handle the new connection, and write a long (and in fact, never-ending) banner
+    
+    // Close the connection when this function ends
+    defer conn.Close()
+    
+    
+    // Main loop - get a random string, write it, sleep then do it again
+    for {
+        var randstr = genString(10)
+        conn.Write([]byte(randstr))
+        conn.Write([]byte("\r\n"))
+        // Sleep for 2s before sending the next
+        time.Sleep(2 * time.Second)
+    }
+}
+
+
+func genString(length int) (string){
+    // Generate a psuedo-random string
+    rand.Seed(time.Now().Unix())
+    charSet := "abcdedfghijklmnopqrstABCDEDFGHIJKLMNOPQRSTUVWXYZ0123456789=.<>?!#@''"
+    
+    var output strings.Builder
+    
+    // Generate the string
+    for i := 0; i < length; i++ {
+        randnum := rand.Intn(len(charSet))
+        randChar := charSet[randnum]
+        output.WriteString(string(randChar))
+    }
+    
+    return output.String()
+    
+}
